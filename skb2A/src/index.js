80524e6d53260995d0de53b745f1c38465cfdb1f
dmitriyprylutskyi <dprily@gmail.com>
2016-11-10 23:20:28 +0200

    Work

--- skb2A/src/index.js
+++ skb2A/src/index.js
@@ -4,11 +4,11 @@ import cors from 'cors';
 const app = express();
 app.use(cors());
 app.get('/', (req, res) => {
-  res.json({
-    hello: 'JS World',
-  });
+  const sum = (+ req.query.a || 0) + (+ req.query.b ||0);
+  res.send(sum.toString());
 });
 
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
+
